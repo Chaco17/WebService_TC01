989278a1f64072275f94065099cae18e2c447053
Sebastián Chacón Muñoz <134745051+Chaco17@users.noreply.github.com>
2025-03-26 17:54:27 -0600

    updt: swagger documentation implementation (initial setup & users)

--- server.js
+++ server.js
@@ -3,10 +3,33 @@ const dotenv = require("dotenv");
 const sequelize = require("./src/config/database");
 const reservationRoutes = require("./src/routes/reservation.routes");
 const orderRoutes = require("./src/routes/order.routes");
+//swagger
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerui = require("swagger-ui-express");
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REST API para un restaurante",
+      version: "1.0.0",
+      description: "Una REST API para un servicio de restauirantes usando PostgreSQL y Express"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: ["./API_PRUEBA/src/routes/*.js"] // Revisar ruta
+}
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+//Ruta de documentación
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerjsdoc(swaggerSpec)));
 
 // Registrar rutas
 app.use("/reservations", reservationRoutes);
