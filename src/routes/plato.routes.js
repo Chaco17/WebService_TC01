a3f9e61c8913fefd29cd39bc9921cf3aff8556fb
Jose Pablo Agüero Mora <joseaguero11@estudiantec.cr>
2025-03-29 19:47:33 -0600

    updt: main change

--- src/routes/plato.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { crearPlato, getPlatoId, actualizarPlato} = require('../controllers/plato.controller');
-
-router.post('/platos', crearPlato);
-router.get('/platos/:id', getPlatoId);
-router.put('/platos/:id', actualizarPlato);
-
-module.exports = router;
